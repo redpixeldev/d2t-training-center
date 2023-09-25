4df6a6ce8f131b4574f9986a5e9281702a4840b0
Vivian Guillen <me@vivian.do>
2023-09-25 11:45:46 -0400

    Fixes

--- src/components/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-	return (
-		<div>
-			<h1 class='text-xl'>Home</h1>
-			<p>This is the JSX homepage page.</p>
-			<img src='/img/icon-home.png' />
-		</div>
-	);
-}
